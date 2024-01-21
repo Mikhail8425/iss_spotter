e8b53fce495d43b62e03d887eb3599eece533f5e
Mikhail8425 <mikhailshemet@gmail.com>
2024-01-20 19:27:11 -0500

    stage 4 half way through

--- iss_promised.js
+++ iss_promised.js
@@ -8,4 +8,24 @@ const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json');
 };
 
+/*
+ * Requests data from https://iss-flyover.herokuapp.com using provided lat/long data
+ * Input: JSON body containing geo data response from ipwho.is
+ * Returns: Promise of request for fly over data, returned as JSON string
+ */
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${ip}`);
+
+};
+
+
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+
 module.exports = { fetchMyIP };
